09a3f6552ede8a4f28c0b6c8d8bf4bd130bfaddd
skchow03 <skchow03@gmail.com>
2025-10-22 09:23:33 -0700

    Add .gitignore to clean up build and cache files

--- timing_app/build.bat
+++
@@ -1,5 +0,0 @@
-pyinstaller --noconfirm --windowed --onefile main.py ^
-  --name ICR2Timing ^
-  --add-data "ui/control_panel.ui;ui" ^
-  --add-data "assets/icon.ico;assets" ^
-  --icon assets/icon.ico
